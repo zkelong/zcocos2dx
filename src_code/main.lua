e69c18fef8540a5dd5e1e0c21cbfa84387d7d4d2
kelong <zkelong@126.com>
2018-03-05 17:41:11 +0800

    lua 垃圾清理设置 设置fps 屏幕适配

--- src_code/main.lua
+++ src_code/main.lua
@@ -3,10 +3,35 @@ cc.FileUtils:getInstance():setPopupNotify(false)
 cc.FileUtils:getInstance():addSearchPath("src_code/")
 cc.FileUtils:getInstance():addSearchPath("res/")
 
-require "config"
+require "config"   -- 调用配置，初始化全局变量
 require "cocos.init"
 
 local function main()
+    collectgarbage("collect")
+    collectgarbage("setpause", 100)
+    collectgarbage("setstepmul", 5000)
+
+    local director = cc.Director:getInstance()
+    local glview = director:getOpenGLView()
+    if nil == glview then
+        glview = cc.GLViewImpl:createWithRect("Cocos2d-Lua", cc.rect(0, 0, 960, 640))
+        director:setOpenGLView(glview)
+    end
+
+    director:setAnimationInterval(1.0 / 30)
+    
+    local designSize = cc.size(960, 640)
+    local resourceSize = cc.size(960, 640)
+    director:setContentScaleFactor(resourceSize.height / designSize.height)
+    local screenSize = glview:getFrameSize()
+    local rate_w = screenSize.width / designSize.width
+    local rate_h = screenSize.height / designSize.height
+    if rate_w > rate_h then
+        glview:setDesignResolutionSize(designSize.width, designSize.height, cc.ResolutionPolicy.FIXED_HEIGHT)
+    else
+        glview:setDesignResolutionSize(designSize.width, designSize.height, cc.ResolutionPolicy.FIXED_WIDTH)
+    end
+
     require("app.MyApp"):create():run()
 end
 
